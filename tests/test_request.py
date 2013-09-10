e2efe636fec91422e539996e62d34e451f97b57f
Silas Barta <sbarta@gmail.com>
2013-09-10 11:50:10 -0700

    cleanup

--- tests/test_request.py
+++ tests/test_request.py
@@ -20,11 +20,6 @@ class TestResult(unittest.TestCase):
         actual = Request().url
         self.assertEqual(expected, actual)
 
-    #def test_with_location(self):
-    #    expected="http://gdata.youtube.com/feeds/api/standardfeeds/most_viewed?alt=json&location=34.05,-118.25&location-radius=50km"
-    #    actual = self.r1.key_elements()
-    #    self.assertEqual(expected, actual)
-
 
 if __name__ == '__main__':
     unittest.main()
